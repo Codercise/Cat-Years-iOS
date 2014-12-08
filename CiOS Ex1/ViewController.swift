83af3bfd9c17d45cc50627b4fcb0d5c74493f0d4
Nick Hayden <nickhayden1992@gmail.com>
2014-12-08 23:03:38 +1030

    completed functionality commit

--- CiOS Ex1/ViewController.swift
+++ CiOS Ex1/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var humanYears: UITextField!
+    @IBOutlet weak var catYearsLabel: UILabel!
+
+    @IBAction func goButtonPressed(sender: AnyObject) {
+        if (humanYears.text.toInt() != nil) {
+            var catYears = humanYears.text.toInt()! * 7
+            //println(catYears)
+            catYearsLabel.text = String(catYears)+" cat years"
+        } else if (humanYears.text.toInt() <= 0) {
+            catYearsLabel.text = "please enter a positive integer"
+        } else { catYearsLabel.text = "error" }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        println("Hello, world")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
